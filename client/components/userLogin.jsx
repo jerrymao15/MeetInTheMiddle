c86ef449e659e65b114a60be4d9eb31c1f8b40ba
Michael-Bryant Choa <mbchoa@gmail.com>
2016-06-06 22:09:08 -0700

    finish unit tests for UserLogin component

--- client/components/userLogin.jsx
+++ client/components/userLogin.jsx
@@ -2,18 +2,21 @@
 const React = require('react');
 
 var UserLogin = React.createClass({
-
   render: function () {
     return (
-      <form>
-        <input id='username' value={this.props.userValue}/>
-        <input id='password' value={this.props.passwordValue}/>
-        <button></button>
+      <form onSubmit={this.props.handleSubmit}>
+        <input
+          id='username'
+          value={this.props.userValue}
+          onChange={this.props.handleUserChange} />
+        <input
+          id='password'
+          value={this.props.passwordValue}
+          onChange={this.props.handlePasswordChange} />
+        <button>Sign Up</button>
       </form>
-
      );
   }
-
 });
 
 module.exports = UserLogin;
--- test/userLogin-Test.js
+++ test/userLogin-Test.js
@@ -5,8 +5,6 @@ import { shallow, mount } from 'enzyme';
 import UserLogin from '../client/components/userLogin';
 
 describe('UserLogin Component', function() {
-//two inputs and a button
-//holds username and password state whilst typing occurs
 
   it('should have one form element', function() {
     expect(shallow(<UserLogin />).find('form').length).to.equal(1);
@@ -19,7 +17,7 @@ describe('UserLogin Component', function() {
   it('should have one button element', function() {
     expect(shallow(<UserLogin />).children('button').length).to.equal(1);
   });
-
+  
   it('should have username value in input element', function() {
     const userVal = mount(<UserLogin userValue={'bjdelro'}/>);
     expect(userVal.find('#username').prop('value')).to.equal('bjdelro');
@@ -30,5 +28,24 @@ describe('UserLogin Component', function() {
     expect(passwordVal.find('#password').prop('value')).to.equal('abc123');
   });
 
+  it('should have form with onSubmit handler', function() {
+    const testHandleSubmitFn = function(e) {}
+    const userLogin = shallow(<UserLogin handleSubmit={testHandleSubmitFn}/>);
+    expect(typeof userLogin.find('form').prop('onSubmit')).to.equal('function');
+    expect(userLogin.find('form').prop('onSubmit')).to.deep.equal(testHandleSubmitFn);
+  });
 
+  it('should have username input with onChange handler', function() {
+    const testOnChangeFn = function(e) {};
+    const userName = mount(<UserLogin handleUserChange={testOnChangeFn}/>).find('#username');
+    expect(typeof userName.prop('onChange')).to.equal('function');
+    expect(userName.prop('onChange')).to.deep.equal(testOnChangeFn);
+  });
+
+  it('should have password input with onChange handler', function() {
+    const testOnChangeFn = function(e) {};
+    const password = mount(<UserLogin handlePasswordChange={testOnChangeFn}/>).find('#password');
+    expect(typeof password.prop('onChange')).to.equal('function');
+    expect(password.prop('onChange')).to.deep.equal(testOnChangeFn);
+  });
 });
