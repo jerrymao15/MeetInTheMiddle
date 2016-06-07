de2b3771b03100c9da470a49d77d0c8b845684c4
Brendan Del Rosario <brendandelrosario@gmail.com>
2016-06-06 20:32:45 -0700

    renamed loginPage to signUp because that makes sense.

--- test/ActivitiesChoice-Test.js
+++ test/ActivitiesChoice-Test.js
@@ -1,7 +1,7 @@
 'use strict';
 import React from 'react';
 import { expect } from 'chai';
-import { shallow, mount, render } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import ActivityChoice from '../client/components/activityChoices';
 
 
---
+++ test/userLogin-Test.js
@@ -0,0 +1,13 @@
+'use strict';
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import UserLogin from '../client/components/userLogin';
+
+describe('UserLogin Component', function() {
+//two inputs and a button
+//holds username and password state whilst typing occurs
+
+
+
+});
