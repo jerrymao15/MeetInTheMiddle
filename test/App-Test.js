65eec8beb3bc5a63c7f993960d310d67393637c9
Brendan Del Rosario <brendandelrosario@gmail.com>
2016-06-07 22:07:17 -0700

    fixed app-test file

--- test/App-Test.js
+++ test/App-Test.js
@@ -7,11 +7,14 @@ import App from '../client/components/app';
 
 describe("Add UserLogin to SignUp page", function() {
   it("should have user login component", function() {
-    expect(mount(<App />).find('UserLogin').length).to.equal(1);
+    const wrapper = mount(<App />);
+    wrapper.setState({currentPage: 'signUpPage'});
+    expect(wrapper.find('UserLogin').length).to.equal(1);
   });
 
-  it('should have 5 props being passed down', function() {
-    const wrapper = mount(<App />).find('UserLogin').props();
-    expect(Object.keys(wrapper).length).to.equal(5);
+  it('should have 5 props being passed down!', function() {
+    const wrapper = mount(<App />);
+    wrapper.setState({currentPage: 'signUpPage'});
+    expect(Object.keys(wrapper.find('UserLogin').props()).length).to.equal(5);
   });
 });
