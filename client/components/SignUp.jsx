d6b4d121352e654c306fa329c6e94b9983f1134c
Michael-Bryant Choa <mbchoa@gmail.com>
2016-06-08 16:59:04 -0700

    create seperate route for login and signup page

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -5,30 +5,37 @@ var SignUp = React.createClass({
 
   render: function () {
     return (
-      <form onSubmit={this.props.onSubmit}>
-        <div className="row">
-          <div className="three columns">
-            <label htmlFor="firstName">First Name:</label>
-            <input type="text" id="firstName" />
+      <div className="sixteen columns">
+        <form
+          onSubmit={this.props.onSubmit}
+          className="four columns offset-by-five">
+          <div className="row">
+            <div className="five columns">
+              <label htmlFor="firstName">First Name</label>
+              <input type="text" id="firstName" />
+            </div>
           </div>
-
-          <div className="three columns">
-            <label htmlFor="lastName">Last Name:</label>
-            <input type="text" id="lastName" />
+          <div className="row">
+            <div className="five columns">
+              <label htmlFor="lastName">Last Name</label>
+              <input type="text" id="lastName" />
+            </div>
           </div>
-
-          <div className="three columns">
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="signup-username" />
+          <div className="row">
+            <div className="five columns">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="signup-username" />
+            </div>
           </div>
-
-          <div className="three columns">
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="signup-password" />
+          <div className="row">
+            <div className="five columns">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="signup-password" />
+            </div>
           </div>
-        </div>
-        <button className="button-primary" >Sign Up</button>
-      </form>
+          <button className="button-primary" >Sign Up</button>
+        </form>
+      </div>
     );
   },
 
