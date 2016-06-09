d6c941e9665ac82d3d493e5243e778333791c271
Jenna <jenna@Jennas-MacBook-Air.local>
2016-06-08 17:29:00 -0700

    updated all error handling to use modal and added test

--- server/server.js
+++ server/server.js
@@ -42,7 +42,6 @@ app.post('/meet',
   dataHandler.sendOutput);
 
 app.post('/createuser', databaseOps.createUser, (req, res) => {
-  console.log(req.body.databaseResponse);
 	if (req.body.databaseResponse.hasOwnProperty('errors')) return res.status(404).end();
 	res.send(req.body.databaseResponse);
 });
